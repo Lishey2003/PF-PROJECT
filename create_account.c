53f778cc05b90603e4532a07f33c0cc8da6750bd
Lishey2003 <168904285+Lishey2003@users.noreply.github.com>
2024-05-12 17:27:39 +0500

    change data type for phone_number

--- create_account.c
+++ create_account.c
@@ -10,7 +10,7 @@ char name[50];
 char date_of_birth[50];
 float age;
 char address[50];
-int phone_number;
+char phone_number[20];
 char account_type[50];
 int deposit_amount;
 };
@@ -40,18 +40,19 @@ void create_account(struct account array[],FILE *file_pointer){
  	puts("Enter Your Full Address : ");
  	gets(array[i].address);
  	printf("Enter Your Mobile Number :");
- 	scanf("%d",&array[i].phone_number);
- 	getchar();
+ 	gets(array[i].phone_number);
  	puts("Enter Your Account_Type : ");
  	gets(array[i].account_type);
  	printf("Enter How Much Money You Want To Deposit :");
  	scanf("%d",&array[i].deposit_amount);
+ 
  	
  	fprintf(file_pointer,"ACCOUNT NUMBER : %d\n",array[i].account_number);
  	fprintf(file_pointer,"NAME : %s\n",array[i].name);
  	fprintf(file_pointer,"DATE OF BIRTH : %s\n",array[i].date_of_birth);
  	fprintf(file_pointer,"AGE : %.1f\n",array[i].age);
  	fprintf(file_pointer,"HOME ADDRESS : %s\n",array[i].address);
+ 	fprintf(file_pointer,"MOBILE NO : %s\n",array[i].phone_number);
  	fprintf(file_pointer,"ACCOUNT TYPE : %s\n",array[i].account_type);
  	fprintf(file_pointer,"DEPOSIT AMOUNT : %d\n",array[i].deposit_amount);
  fclose (file_pointer);
