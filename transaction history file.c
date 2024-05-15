831126e61f403119acb91149d7720db264613ab5
Lishey2003 <168904285+Lishey2003@users.noreply.github.com>
2024-05-15 23:59:52 +0500

    correct transaction history code

--- transaction history file.c
+++ transaction history file.c
@@ -1,8 +1,9 @@
-#include<stdio.h>
-#include<string.h>
 
-int account_no = 213450 ;
-int i = 0 ;
+#include<stdio.h>
+
+
+int account_no = 213450;
+int i = 0;
 
 struct account{
 int account_number;
@@ -10,164 +11,20 @@ char name[50];
 char date_of_birth[50];
 float age;
 char address[50];
-int phone_number;
+char phone_number[20];
 char account_type[50];
 int deposit_amount;
-};
-
-
-   
-	
-void create_account(struct account array[],FILE *file_pointer);
-void transaction(struct account array[],FILE *fptr,FILE *file_pointer);
-void check_history(struct account array[],FILE *fptr);
-
-
-void create_account(struct account array[],FILE *file_pointer){
-
-
-    file_pointer = fopen("Bank.txt", "a");
-    if (file_pointer == NULL) {
-        printf("Error in opening file:");
-    }
- else{
-    printf("\nFile opened:");
-  	printf("\n*****************WELCOME*******************");
-  	printf("\nFor Creating Account Provide The Following Information :");
- 	array[i].account_number = account_no + i;
- 	getchar();
- 	puts("\nEnter Your Full Name : ");
- 	gets(array[i].name);
- 	puts("Enter Your Date Of Birth");
- 	gets(array[i].date_of_birth);
- 	printf("Enter Your Age :");
- 	scanf("%f",&array[i].age);
- 	getchar();
- 	puts("Enter Your Full Address : ");
- 	gets(array[i].address);
- 	printf("Enter Your Mobile Number :");
- 	scanf("%d",&array[i].phone_number);
- 	getchar();
- 	puts("Enter Your Account_Type : ");
- 	gets(array[i].account_type);
- 	printf("Enter How Much Money You Want To Deposit :");
- 	scanf("%d",&array[i].deposit_amount);
- 	
- 	fprintf(file_pointer,"ACCOUNT NUMBER : %d\n",array[i].account_number);
- 	fprintf(file_pointer,"NAME : %s\n",array[i].name);
- 	fprintf(file_pointer,"DATE OF BIRTH : %s\n",array[i].date_of_birth);
- 	fprintf(file_pointer,"AGE : %.1f\n",array[i].age);
- 	fprintf(file_pointer,"HOME ADDRESS : %s\n",array[i].address);
- 	fprintf(file_pointer,"ACCOUNT TYPE : %s\n",array[i].account_type);
- 	fprintf(file_pointer,"DEPOSIT AMOUNT : %d\n",array[i].deposit_amount);
- 	fclose(file_pointer);
-
-}
-    i++;
-    FILE *fptr;
-   fptr = fopen("transaction history.txt", "w");
-   fclose(fptr);
-    transaction(array,fptr,file_pointer);
-	return;
-	
-} 
-
-
-void transaction(struct account array[],FILE *fptr,FILE *file_pointer){
-	
-	int account_verifaction;
-	printf("\n\n***** WELCOME *****");
-	printf("\nEnter Your Account Number For Transaction :");
-	scanf("%d",&account_verifaction);
-	
-	file_pointer = fopen("Bank.txt", "r");
-	fptr = fopen("transaction history.txt", "a");
-	
-	
-    if (file_pointer == NULL || fptr == NULL) {
-        printf("Error opening file.");
-        return;
-    }
-	
-	 fseek(file_pointer, 0, SEEK_END);
-	 
-	 int found=0;
-	
-	for(int j=0 ; j<i ; j++ ){
-	if(account_verifaction==array[j].account_number){
-		found=1;
-		int payment_choice;
-		printf("Enter : 1.FOR DEPOSIT \t 2.FOR WITHDRAW");
-		scanf("%d",&payment_choice);
-		switch(payment_choice){
-		case 1:
-		printf("\n\nIn your account current balance is : %d\n",array[j].deposit_amount);
-		printf("Enter How Much Money You Want To deposit :");
-		int money;
-		scanf("%d",&money);
-		getchar();
-		char deposit_date[40];
-		printf("Enter deposit date :");
-		gets(deposit_date);
-		
-	    array[j].deposit_amount = array[j].deposit_amount + money ;
-	    
-	    printf("\n\nyou deposit %d money on this date:%s\n",money,deposit_date);
-	    printf("Now your total amount in account is : %d",array[j].deposit_amount); 
-	    
-	    fprintf(fptr,"**** DEPOSIT MONEY ****\tDate:%s\n",deposit_date);
-	    fprintf(fptr,"you deposit %d money on this date:%s\n",money,deposit_date);
-	    fprintf(fptr,"Now your total amount in account is : %d\n",array[j].deposit_amount); 
-		break;
-			
-		case 2:	
-		printf("\n\nIn your account current balance is : %d",array[j].deposit_amount);
-		printf("\nEnter How Much Money You Want To withdraw :");
-		int withdraw_money;
-		scanf("%d",&withdraw_money);
-		getchar();
-		char withdraw_date[40];
-		printf("Enter withdraw date :");
-		gets(withdraw_date);
-		
-	    array[j].deposit_amount = array[j].deposit_amount - withdraw_money ;
-	    
-		printf("\n\nyou withdraw %d money on this date:%s\n",withdraw_money,withdraw_date);
-	    printf("Now your remaining total amount in account is  %d",array[j].deposit_amount); 
-	    		
-	  
-	    fprintf(fptr,"**** withdraw MONEY ****\tDate:%s\n",withdraw_date);
-	    fprintf(fptr,"you withdraw %d money on this date:%s\n",withdraw_money,withdraw_date);
-	    fprintf(fptr,"Now your remaining total amount in account is  %d\n",array[j].deposit_amount); 
-	    
-	
-		break;
-				
-		default:
-		printf("Wrong Payment choice Try Again :");
-		}
-		break;
-	}
 }
 
-if(found==0){
-	printf("****SORRY INCORRECT ACCOUNT NUMBER TRY AGAIN****");
-}
-	fclose(file_pointer); 
-    fclose(fptr);
-    
-    check_history(array,fptr);
-    
-}
+void history(FILE *fptr);
 
-
-
-
-void check_history(struct account array[],FILE *fptr){
+void history(FILE *fptr){
 	
 	 int account_verification;
 	printf("\nEnter Your Account Number For checking Transaction history :");
 	scanf("%d",&account_verification);
+	
+	int account;
 	
 	char detail[500];
 	
@@ -178,32 +35,25 @@ void check_history(struct account array[],FILE *fptr){
     }
     
     int verify=0;
-    for (int j = 0; j < i; j++) {
-        if(account_verification == array[j].account_number) {
-        	verify=1;
-        	printf("\n\nYour transaction history is :\n");
-            while (fgets(detail, sizeof(detail), fptr) != NULL) {
-                printf("%s", detail);
-            }
-            break;
-        }
-    }
     
-    if(verify==0){
-    	printf("SORRY! Inccorect Account Number :");
-	}
+    while(fscanf(fptr,"%d",&account) != EOF) {
+    	if(account==account_verification){
+    	fgets(detail, sizeof(detail), fptr);
+    	printf("%s",detail);
+        verify=1;	
+        }
+		}
+        
+        if(verify==0){
+			printf("account not found");
+		}
+	
 
     fclose(fptr);
 }
 
-
 int main(){
-	struct account array[i];
 	
-    FILE *file_pointer;
-     file_pointer = fopen("Bank.txt", "w");
-     fclose(file_pointer);
-   create_account(array,file_pointer);
-  
-	return 0;
+	history(fptr);
 }
+
